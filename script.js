69a510d93850f6baa873d2b82328fc8f205760a6
somil jain <89907422+somiljain2006@users.noreply.github.com>
2025-08-22 18:45:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const apikey = "fFLPc43asd3NnzuvirZgyqtbwfbj3cd7LoHuhLZY";
+const titl = document.getElementById("title");
+const img = document.getElementById("image");
+const disc = document.getElementById("description");
+const load = document.getElementById("loading");
+
+let curr = new Date();
+curr.setDate(curr.getDate() - 1);
+
+const format = date => date.toISOString().split("T")[0];
+
+async function apod(date) {
+  load.textContent = "Preparing your cosmic view...";
+  img.style.display = "none";
+  try {
+    const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apikey}&date=${date}`);
+    if (!res.ok) throw new Error("Error 404: Future under construction Come back later.");
+    const data = await res.json();
+    titl.textContent = data.title;
+    disc.textContent = data.explanation;
+    img.classList.remove("loaded");
+    img.src = data.url.replace(/^http:/, "https:");
+    img.addEventListener("load", () => {
+      img.classList.add("loaded");
+      load.textContent = "";
+    });
+    img.style.display = "block";
+    } catch (err) {
+        load.textContent = err.message;
+    }
+};
+document.getElementById("prev").addEventListener("click", () => {
+  curr.setDate(curr.getDate() - 1);
+  apod(format(curr));
+});
+
+document.getElementById("next").addEventListener("click", () => {
+  curr.setDate(curr.getDate() + 1); 
+  apod(format(curr));
+});
+
+apod(format(curr));
+
+function reveal() {
+  const rect = disc.getBoundingClientRect();
+  const height = window.innerHeight;
+
+  if (rect.top < height * 0.8) {
+    disc.classList.add("visible");
+    disc.classList.add("card-hover");
+  } else {
+    disc.classList.remove("visible"); 
+    disc.classList.remove("card-hover");
+  }
+}
+window.addEventListener("scroll", reveal);
